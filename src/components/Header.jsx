44afad2e4e7443c92b4fe96c37255be332ae326a
Ryan Bagus <ryanbagusbimantoro@gmail.com>
2024-09-14 13:48:14 +0700

    Add icon to logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import AddNoteItemButton from "./buttons/AddNoteItemButton";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBook } from '@fortawesome/free-solid-svg-icons';
 
 const Header = ({ onAddNoteHandler }) => {
   return (
     <div className="header">
-      <h1 className="header__logo">NoteApp.</h1>
+      <h1 className="header__logo">
+        <FontAwesomeIcon icon={faBook} />
+        NoteApp.
+      </h1>
       <AddNoteItemButton onAddNoteHandler={onAddNoteHandler} />
     </div>
   );
