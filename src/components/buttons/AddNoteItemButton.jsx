e30c0a919c69620b4ac8f51d0aa0f5c40faae81a
Ryan Bagus <ryanbagusbimantoro@gmail.com>
2024-09-14 12:32:54 +0700

    Add adding note item button component

--- src/components/buttons/AddNoteItemButton.jsx
+++ src/components/buttons/AddNoteItemButton.jsx
@@ -4,7 +4,7 @@ import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 const AddNoteItemButton = ({ onAddNoteHandler }) => {
   return (
-    <button onClick={onAddNoteHandler}>
+    <button className='header__add-note' onClick={onAddNoteHandler}>
       <FontAwesomeIcon icon={faPlus} />
       Tambah
     </button>
