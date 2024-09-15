95d9ee8903354424917f0a75885801c947fe8a1f
Ryan Bagus <ryanbagusbimantoro@gmail.com>
2024-09-15 19:12:45 +0700

    Add Delete Note Item Button

--- src/components/buttons/DeleteNoteItemButton.jsx
+++ src/components/buttons/DeleteNoteItemButton.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
-const DeleteNoteItemButton = () => {
-    return <button className='note-item__delete'>Delete</button>;
+const DeleteNoteItemButton = ({ id, onDeleteNote }) => {
+    return (
+      <button onClick={() => onDeleteNote(id)} className='note-item__delete'>
+        <FontAwesomeIcon icon={faTrash} title="Hapus" />
+      </button>
+    );
 };
 
 export default DeleteNoteItemButton;
